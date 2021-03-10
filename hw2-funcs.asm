5b2f5107764561fd8da8777ec7d730bb661f7d65
Kevin Li <kevinli4321@gmail.com>
2021-03-10 12:54:34 -0500

    Phew - finally done converting argument to digits.

--- hw2-funcs.asm
+++ hw2-funcs.asm
@@ -5,10 +5,110 @@
 ############################## Do not .include any files! #############################
 
 .text
-eval: # (string aexp)
-	jr $ra
+eval: # (string AExp)
+	lw $s0, 0($a1)		# Load AExp
+	lbu $s1, 0($s0)		# Load one character from AExp, use $s1 as $t0 would just get overwritten
+	li $s2, 0		# Use $s2 as tp argument
+	iterateAExp:
+		# Check if character is a digit
+		addi $sp, $sp, -4	# Save $ra as I will be calling a secondary function
+		sw $ra, 0($sp)
+		move $a0, $s1		# Make first argument of is_digit the character
+		jal is_digit
+		lw $ra, 0($sp)		# Restore $ra
+		addi $sp, $sp, 4
+		bne $v0, $0, digitFound	# is_digit returned 1, jump ahead and add to value stack
+		
+		digitFound:	# Find additional digits (if any), then push to val_stack
+			addi $sp, $sp, -4
+			sw $ra, 0($sp)
+			move $a0, $s0
+			jal constructOperand
+			lw $ra, 0($sp)
+			addi $sp, $sp, 4
+			
+			# Push val to stack
+			addi $sp, $sp, -4
+			sw $ra, 0($sp)
+			move $a0, $v0
+			move $a1, $s2
+			la $a2, val_stack
+			jal stack_push
+			lw $ra, 0($sp)
+			addi $sp, $sp, 4
+			
+			addi $v1, $v1, -1		# Advance AExp by (n - 1), n being length of val
+			add $s0, $s0, $v1
+			j advanceLoop
 
+		advanceLoop:
+			addi $s0, $s0, 1		# Advance AExp forward by 1 character
+			lbu $s1, 0($s0)		
+			bnez $s1, iterateAExp		# Keep going until null terminator is reached
+		
+	moveForward:
+		jr $ra
 
+constructOperand: # Helper method for eval, takes the AExp as argument
+	# First, find length of operand
+	addi $sp, $sp, -16
+	sw $s0, 0($sp)		# Need to save $a0 before calling findLengthLoop
+	sw $s1, 4($sp)		# Need to save length of string before calling findLengthLoop
+	sw $s2, 8($sp)		# Need to save current character before calling findLengthLoop
+	sw $s3, 12($sp)		# Need to store actual value of all the digits
+	
+	move $s0, $a0		# ^
+	li $s1, 0		# ^
+	findLengthLoop:
+		addi $s1, $s1, 1	# Increment length
+		addi $s0, $s0, 1	# Advance AExp string pointer
+		lbu $s2, 0($s0)		# Load next character in AExp
+		
+		addi $sp, $sp, -4
+		sw $ra, 0($sp)
+		move $a0, $s2
+		jal is_digit
+		lw $ra, 0($sp)		# Restore $ra
+		addi $sp, $sp, 4
+		
+		bne $v0, $0, findLengthLoop	# As long as the digits continue being digits, keep going
+		
+	# By this point in the code, length of string is stored in $s1
+	li $s3, 0		# To store actual value of all the digits
+	li $t0, 0		# findLengthLoop looped right, now loop left
+	li $t1, 10		# Constant 10
+	findValueLoop:
+		addi $s0, $s0, -1	# Decrement AExp string pointer
+		lbu $s2, 0($s0)		# Load next character in AExp (but backwards)
+	
+		li $t2, 1	# Multiply this however many times by 10 for each digit
+		move $t3, $t0	# Don't want to modify the amount of times to apply x10 in this next loop
+		exponent10:
+			beq $t3, $0, goOnToNextLoop
+			mult $t2, $t1
+			mflo $t2
+			addi $t3, $t3, -1
+			bne $t3, $0, exponent10
+			
+		goOnToNextLoop:
+		addi $s2, $s2, -48	# Find actual value of digit
+		mult $s2, $t2		# Multiply it by the appropriate power of 10
+		mflo $t2		# $t2 doesn't matter now so store result here
+		add $s3, $s3, $t2	# Add to total sum
+		
+		addi $t0, $t0, 1		# Move on to digit left of this one
+		bne $t0, $s1, findValueLoop	# Once $t0 reaches the length, stop
+	
+	move $v0, $s3	# Actual value
+	move $v1, $s1	# Length of that value
+	lw $s0, 0($sp)	
+	lw $s1, 4($sp)		
+	lw $s2, 8($sp)		
+	lw $s3, 12($sp)	# Deallocate
+	addi $sp, $sp, 16
+	jr $ra	
+	
+# ======================================================================================================
 
 is_digit: # (char c)
 	li $v0, 0	# Assume character isn't digit initially
